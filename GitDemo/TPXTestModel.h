909199a9a6a8e1d85cea31cbf047db51650f9e2a
PixelHacker <ray@pixelhacker.com>
2014-01-04 12:06:00 -0800

    Added Test Class

---
+++ GitDemo/TPXTestModel.h
@@ -0,0 +1,13 @@
+//
+//  TPXTestModel.h
+//  GitDemo
+//
+//  Created by pixelhacker on 1/4/14.
+//  Copyright (c) 2014 tinypixel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TPXTestModel : NSObject
+
+@end
---
+++ GitDemo/TPXTestModel.m
@@ -0,0 +1,13 @@
+//
+//  TPXTestModel.m
+//  GitDemo
+//
+//  Created by pixelhacker on 1/4/14.
+//  Copyright (c) 2014 tinypixel. All rights reserved.
+//
+
+#import "TPXTestModel.h"
+
+@implementation TPXTestModel
+
+@end
